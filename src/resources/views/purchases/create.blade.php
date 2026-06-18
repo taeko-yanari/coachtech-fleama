5a99be65cff0e950daba850320a7acc74d0fa53b
Yanari Taeko <tae.ienari@gmail.com>
2026-06-19 01:33:07 +0900

    購入関連ビューのインデントを整理

--- src/resources/views/purchases/create.blade.php
+++ src/resources/views/purchases/create.blade.php
@@ -17,16 +17,16 @@
         </div>
       </div>
       <form action="{{ route('purchase.store', $item->id) }}" method="post" class="purchase__form" id="purchase__form">
-          @csrf
-          <div class="purchase__payment-method">
-            <h2 class="purchase__section-title">支払い方法</h2>
-        
-              <div class="payment-method__wrapper">
+        @csrf
+        <div class="purchase__payment-method">
+          <h2 class="purchase__section-title">支払い方法</h2>
+          
+          <div class="payment-method__wrapper">
             <select name="payment_method" id="payment_method" class="payment-method__select">
               <option value="">選択してください</option>
               <option value="コンビニ支払い" {{ old('payment_method', session('payment_method')) === 'コンビニ支払い' ? 'selected' : '' }}>コンビニ支払い</option>
               <option value="カード支払い" {{ old('payment_method', session('payment_method')) === 'カード支払い' ? 'selected' : '' }}>カード支払い</option>
-          </select>
+            </select>
           </div>
           @if ($errors->has('payment_method'))
           <ul class="error-messages">
@@ -34,44 +34,43 @@
             <li>{{ $message }}</li>
             @endforeach
           </ul>
-        @endif
-          
-        
-      </div>
-      <div class="purchase__shipping-address">
-        <div class="shipping-address__info">
-          <h2 class="purchase__section-title">配送先</h2>
-          <div class="purchase__shipping-address__text">
-            <p class="postal_code">{{ $shipping_postal_code }}</p>
-            <input type="hidden" name="shipping_postal_code" value="{{ $shipping_postal_code }}">
-            <p class="address">{{ $shipping_address }}  {{ $shipping_building }}</p>
-            <input type="hidden" name="shipping_address" value="{{ $shipping_address }}">
-            <input type="hidden" name="shipping_building" value="{{ $shipping_building }}">
-          </div>
-          @if ($errors->has('shipping_postal_code'))
-          <ul class="error-messages">
-            @foreach ($errors->get('shipping_postal_code') as $message)
-            <li>{{ $message }}</li>
-            @endforeach
-          </ul>
-        @endif
+          @endif
+        </div>
+        <div class="purchase__shipping-address">
+          <div class="shipping-address__info">
+            <h2 class="purchase__section-title">配送先</h2>
+            <div class="purchase__shipping-address__text">
+              <p class="postal_code">{{ $shipping_postal_code }}</p>
+              <input type="hidden" name="shipping_postal_code" value="{{ $shipping_postal_code }}">
+              <p class="address">{{ $shipping_address }}  {{ $shipping_building }}</p>
+              <input type="hidden" name="shipping_address" value="{{ $shipping_address }}">
+              <input type="hidden" name="shipping_building" value="{{ $shipping_building }}">
+            </div>
 
-           @if ($errors->has('shipping_address'))
-          <ul class="error-messages">
-            @foreach ($errors->get('shipping_address') as $message)
-            <li>{{ $message }}</li>
-            @endforeach
-          </ul>
-        @endif
+            @if ($errors->has('shipping_postal_code'))
+            <ul class="error-messages">
+              @foreach ($errors->get('shipping_postal_code') as $message)
+              <li>{{ $message }}</li>
+              @endforeach
+            </ul>
+            @endif
+                
+            @if ($errors->has('shipping_address'))
+            <ul class="error-messages">
+              @foreach ($errors->get('shipping_address') as $message)
+              <li>{{ $message }}</li>
+              @endforeach
+            </ul>
+            @endif
+          </div>
+
+          <div class="shipping-address__edit">
+            <a href="{{ route('purchase.address.edit', [$item->id, 'payment_method' => session('payment_method')]) }}" class="shipping-address__edit-link">変更する</a>
+          </div>
         </div>
-        <div class="shipping-address__edit">
-          <a href="{{ route('purchase.address.edit', [$item->id, 'payment_method' => session('payment_method')]) }}" class="shipping-address__edit-link">変更する</a>
-          
-        </div>
-      </div>
       </form>
     </div>
-
+        
     <div class="purchase__flex-right">
       <div class="purchase__info">
         <table class="purchase__table">
@@ -84,7 +83,7 @@
             <td class="table__text table__payment-method"></td>
           </tr>
         </table>
-
+            
         <div class="purchase__button">
           <button type="submit" class="purchase__button-submit" form="purchase__form">購入する</button>
         </div>
--- src/resources/views/purchases/edit_address.blade.php
+++ src/resources/views/purchases/edit_address.blade.php
@@ -4,50 +4,50 @@
 @endsection
 
 @section('content')
-  <div class="container">
-    <h1 class="edit-address__title">住所の変更</h1>
-    <form action="{{ route('purchase.address.update', $item->id) }}" method="post" class="edit-address__form">
-      @csrf
-      <div class="edit-address__group">
-        <label for="shipping_postal_code" class="edit-address__label">郵便番号</label>
-        <input type="text" name="shipping_postal_code" id="shipping_postal_code" class="edit-address__input" value="{{ old('shipping_postal_code') }}">
+<div class="container">
+  <h1 class="edit-address__title">住所の変更</h1>
+  <form action="{{ route('purchase.address.update', $item->id) }}" method="post" class="edit-address__form">
+    @csrf
+    <div class="edit-address__group">
+      <label for="shipping_postal_code" class="edit-address__label">郵便番号</label>
+      <input type="text" name="shipping_postal_code" id="shipping_postal_code" class="edit-address__input" value="{{ old('shipping_postal_code') }}">
+      
+      @if ($errors->get('shipping_postal_code'))
+      <ul class="error-messages">
+        @foreach ($errors->get('shipping_postal_code') as $message)
+          <li>{{ $message }}</li>
+        @endforeach
+      </ul>
+      @endif
+    </div>
 
-        @if ($errors->get('shipping_postal_code'))
-          <ul class="error-messages">
-            @foreach ($errors->get('shipping_postal_code') as $message)
-              <li>{{ $message }}</li>
-            @endforeach
-          </ul>
-        @endif
-      </div>
+    <div class="edit-address__group">
+      <label for="shipping_address" class="edit-address__label">住所</label>
+      <input type="text" name="shipping_address" id="shipping_address" class="edit-address__input" value="{{ old('shipping_address') }}" >
+      
+      @if ($errors->get('shipping_address'))
+      <ul class="error-messages">
+        @foreach ($errors->get('shipping_address') as $message)
+          <li>{{ $message }}</li>
+        @endforeach
+      </ul>
+      @endif
+    </div>
 
-      <div class="edit-address__group">
-        <label for="shipping_address" class="edit-address__label">住所</label>
-        <input type="text" name="shipping_address" id="shipping_address" class="edit-address__input" value="{{ old('shipping_address') }}" >
+    <div class="edit-address__group">
+      <label for="shipping_building" class="edit-address__label">建物名</label>
+      <input type="text" name="shipping_building" id="shipping_building" class="edit-address__input" value="{{ old('shipping_building') }}">
 
-        @if ($errors->get('shipping_address'))
-          <ul class="error-messages">
-            @foreach ($errors->get('shipping_address') as $message)
-              <li>{{ $message }}</li>
-            @endforeach
-          </ul>
-        @endif
-      </div>
+      @if ($errors->get('shipping_building'))
+      <ul class="error-messages">
+        @foreach ($errors->get('shipping_building') as $message)
+        <li>{{ $message }}</li>
+        @endforeach
+      </ul>
+      @endif
+    </div>
 
-      <div class="edit-address__group">
-        <label for="shipping_building" class="edit-address__label">建物名</label>
-        <input type="text" name="shipping_building" id="shipping_building" class="edit-address__input" value="{{ old('shipping_building') }}">
-
-        @if ($errors->get('shipping_building'))
-          <ul class="error-messages">
-            @foreach ($errors->get('shipping_building') as $message)
-              <li>{{ $message }}</li>
-            @endforeach
-          </ul>
-        @endif
-      </div>
-
-      <button type="submit" class="edit-address__submit">更新する</button>
-    </form>
-  </div>
+    <button type="submit" class="edit-address__submit">更新する</button>
+  </form>
+</div>
 @endsection
